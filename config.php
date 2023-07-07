8d1f71a85fadb36c33e5c751f7a56186796ba769
Bruno Agenor <contato@brunoage.dev>
2023-07-07 15:36:26 -0300

    upload config.php

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = getenv('MOODLE_DATABASE_TYPE');
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = getenv('MOODLE_DATABASE_HOST');
+$CFG->dbname    = getenv('MOODLE_DATABASE_NAME');
+$CFG->dbuser    = getenv('MOODLE_DATABASE_USER');
+$CFG->dbpass    = getenv('MOODLE_DATABASE_PASSWORD');
+$CFG->prefix    = getenv('MOODLE_DATABASE_PREFIX');
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => getenv('MOODLE_DATABASE_PORT_NUMBER'),
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_unicode_ci',
+);
+
+$CFG->wwwroot   = getenv('MOODLE_HOST');
+$CFG->dataroot  = getenv('MOODLEDATA_PATH');
+$CFG->admin     = getenv('MOODLE_USERNAME');
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
